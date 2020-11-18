31e72e89d54df8174b501b52496f6eaef1f8c7fd
tjkr0wn <joumaa_family@icloud.com>
2020-11-17 20:03:30 -0500

    Added first patchset, to be tested

--- xnu_gym/bugs/task_for_pid.c
+++ xnu_gym/bugs/task_for_pid.c
@@ -8,9 +8,13 @@ void tfp0_all_callback(uint32_t *err) {
   pretty_log("Doing tfp0 patches. Transferring output to match handlers...", INFO);
 
   xnu_pf_patchset_t *patchset = xnu_pf_patchset_create(XNU_PF_ACCESS_32BIT);
+  struct mach_header_64 *main_header =  xnu_header();
+  struct segment_command_64 *TEXTEXEC = macho_get_segment(main_header, "__TEXT_EXEC");
 
   uint64_t task_for_pid_posix_check_opcodes[] = {
-
+    0x130dee87,     /* bl sub_xxx */
+    0x88d038d5,     /* mrs x_, tpidr_el1 */
+    0x088d41f9      /* ldr x8, [xn, #0xn]*/
   };
 
   const size_t task_for_pid_posix_check_opcodes_c =
@@ -18,11 +22,9 @@ void tfp0_all_callback(uint32_t *err) {
       sizeof(*task_for_pid_posix_check_opcodes);
 
   uint64_t task_for_pid_posix_check_masks[] = {
-    0xffffffff,     /* match exactly */
     0xfc000000,     /* ignore immediate */
-    0xffffffff,     /* match exactly */
-    0xffc003ff,     /* ignore immediate */
-    0xffc003ff,     /* ignore immediate */
+    0xffffffe0,     /* ignore x_ in mrs */
+    0xffc0001f      /*  ignore [] */
   };
 
   xnu_pf_maskmatch(
@@ -32,22 +34,26 @@ void tfp0_all_callback(uint32_t *err) {
             task_for_pid_posix_check_opcodes_c,
             false,
             handle_task_for_pid_posix_check
-          );
+  );
 
+  xnu_pf_emit(patchset);
+  xnu_pf_apply(TEXTEXEC, patchset);
+  xnu_pf_patchset_destroy(patchset);
 
+  
   return;
 }
 
 static bool handle_task_for_pid_posix_check(struct xnu_pf_patch* patch, void* cacheable_stream) {
-  printf("%s: Started\n", __func__);
+  printf("\t%s: Started\n", __func__);
   xnu_pf_disable_patch(patch);
-  
+
 
   return;
 }
 
 static bool handle_task_conversion_eval(struct xnu_pf_patch* patch, void* cacheable_stream) {
-  printf("%s: Started\n", __func__);
+  printf("\t%s: Started\n", __func__);
   xnu_pf_disable_patch(patch);
 
 
@@ -55,7 +61,7 @@ static bool handle_task_conversion_eval(struct xnu_pf_patch* patch, void* cachea
 }
 
 static bool handle_inline_task_for_pid_check(struct xnu_pf_patch* patch, void* cacheable_stream) {
-  printf("%s: Started\n", __func__);
+  printf("\t%s: Started\n", __func__);
   xnu_pf_disable_patch(patch);
 
 
