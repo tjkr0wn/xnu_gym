7e6698786550c2b0b3f3e80fec0a4ccfe9d4f421
CodeJou <joumaa_Family@icloud.com>
2020-11-18 11:58:04 -0500

    Added useful macros

--- include/xnu_gym.h
+++ include/xnu_gym.h
@@ -1,9 +1,13 @@
 #ifndef xnu_gym_h
 #define xnu_gym_h
 
+#define ARG_EXISTS(a, s) (memmem(a, strlen(a), s, strlen(s))
+#define DEBUG(m) (pretty_log(m, INFO))
+
 struct bug_t {
   void (*cb)(uint32_t *error_counter);
   struct bug_t *next;
 };
+
 
 #endif /* xnu_gym_h */
