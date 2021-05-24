5eed0f8489642ccd86473d43a507ab997c9da90b
tjkr0wn <joumaa_family@icloud.com>
2021-05-23 20:19:16 -0400

    cleaned up

--- include/xnu_gym.h
+++ include/xnu_gym.h
@@ -2,9 +2,7 @@
 #define xnu_gym_h
 
 #define ARG_EXISTS(a, s) (memmem(a, strlen(a), s, strlen(s))
-#define DEBUG(m) (pretty_log(m, DG))
 #define NEWENTRY(a, cb) (else if (ARG_EXISTS(args, a)) init_new_patch(cb))
-//#define DEBUG(m) (asm volatile("mov x0, x0\n"))
 
 struct bug_t {
   int (*cb)();
